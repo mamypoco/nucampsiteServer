de42c1d269ac76c5c0e565c8da7ea500ca3a1540
Mami Nishiwaki <mamypoco@gmail.com>
2022-06-23 15:52:59 -0700

    Express REST API with MongoDB and Mongoose Part 1

--- app.js
+++ app.js
@@ -11,6 +11,20 @@ var usersRouter = require("./routes/users");
 // const promotionRouter = require('./routes/campsiteRouter');
 // const partnerRouter = require('/routes/partnerRouter');
 
+const mongoose = require("mongoose");
+const url = "mongodb://localhost:27017/nucampsite";
+const connect = mongoose.connect(url, {
+   useCreateIndex: true,
+   useFindAndModify: false,
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+});
+
+connect.then(
+   () => console.log("Connected correctly to server"),
+   (err) => console.log(err)
+);
+
 var app = express();
 
 // view engine setup
